d89ef57a2c2eb5a18339f31f89caef34b7bc2c52
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-02-12 15:38:39 +0100

    organize aliases

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,21 +1,28 @@
-alias l='ls -lah --color=auto'
 alias sourcezsh='source ~/.zshrc'
+alias cat='bat --style=plain --paging=never'
+alias ccat='\cat'               # original cat command
+
+# Tools
 alias v='nvim'
 alias ff='fastfetch'
 alias lg='lazygit'
 alias y='yazi'
-alias ta='tmux attach'
-alias t='tmux'
-alias tk='tmux kill-server'
-alias tl='tmux ls'
-alias chd='chezmoi cd'
-alias chra='chezmoi re-add'
-alias cha='chezmoi add'
 alias oc='opencode'
 
+# Coloring
+alias l='ls -lah --color=auto'
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias ip='ip --color=auto'
 
-alias cat='bat --style=plain --paging=never'
-alias ccat='\cat'               # original cat command
+# Chezmoi
+alias chd='chezmoi cd'
+alias chra='chezmoi re-add'
+alias cha='chezmoi add'
+alias chap='chezmoi apply'
+
+# Tmux
+alias ta='tmux attach'
+alias t='tmux'
+alias tk='tmux kill-server'
+alias tl='tmux ls'
