58357507648dfc49bd008c2cdd9fddb107e77222
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-02-13 12:09:35 +0100

    fix zsh config

--- dot_config/zsh/zshbinds.zsh
+++ dot_config/zsh/zshbinds.zsh
@@ -1,4 +1,6 @@
 bindkey "^[[1;5C" forward-word      # Ctrl+Right
+bindkey "^[[1;2C" forward-word      # Ctrl+Right
 bindkey "^[[1;5D" backward-word     # Ctrl+Left
+bindkey "^[[1;2D" backward-word     # Ctrl+Left
 bindkey "^y" autosuggest-accept
 bindkey -s "^f" " tmux-sessionizer\n"
