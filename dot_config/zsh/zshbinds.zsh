8924f4e392767834733a10cab20c38e62c3da865
Kiryl Kvoit <kiryl.kvit@gmail.com>
2026-02-14 11:03:58 +0100

    fix zsh config

--- dot_config/zsh/zshbinds.zsh
+++ dot_config/zsh/zshbinds.zsh
@@ -3,6 +3,7 @@ bindkey "^[[1;2C" forward-word      # Ctrl+Right
 bindkey "^[[1;5D" backward-word     # Ctrl+Left
 bindkey "^[[1;2D" backward-word     # Ctrl+Left
 bindkey "^y" autosuggest-accept
+bindkey "^e" autosuggest-accept     # Ctrl+E
 bindkey -s "^f" " tmux-sessionizer\n"
 
 # Esc Esc: add/toggle sudo for current line (or last cmd)
