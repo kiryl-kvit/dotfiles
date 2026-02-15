c2a02df2e2a930d4954e8b3e136fb2955c89bb5d
Kiryl Kvoit <kiryl.kvit@gmail.com>
2026-02-15 14:39:31 +0100

    fix zsh autocompletions

--- dot_config/zsh/zshconfig.zsh
+++ dot_config/zsh/zshconfig.zsh
@@ -8,8 +8,12 @@ fi
 
 export LESS='-R'
 
+autoload -Uz compinit
+compinit
+
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
 zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
 zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
